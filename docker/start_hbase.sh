a39fe19e51ed7efcec544fc7a49a3d80f1cc2f6b
Daniel Schneller <ds@danielschneller.de>
2017-12-08 16:39:09 +0100

    Pass Container Env to start scripts

--- docker/start_hbase.sh
+++ docker/start_hbase.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/with-contenv /bin/bash
 export JAVA_HOME="::JAVA_HOME::"
 export PATH="::PATH::"
 trap "echo stopping hbase;/opt/hbase/bin/hbase master stop>>/var/log/hbase-stop.log 2>&1; exit" HUP INT TERM EXIT
